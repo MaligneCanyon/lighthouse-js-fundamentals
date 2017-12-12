e14191f8e6881b9178ce963b12c5e343ded26381
MaligneCanyon <grantkaratnyk@hotmail.com>
2017-12-12 05:06:26 +0000

    added functions exercises

---
+++ even.js
@@ -0,0 +1,15 @@
+function isEven(num) {
+  return num % 2 === 0; // return T or F
+}
+
+/*
+var tenIsEven = isEven(10);
+var elevenIsEven = isEven(11);
+
+console.log(tenIsEven);
+console.log(elevenIsEven);
+*/
+
+// simpler:
+console.log(isEven(10));
+console.log(isEven(11));
---
+++ hello.js
@@ -0,0 +1,7 @@
+function sayHello(name) {
+  console.log("Hello, " + name); // new tradition
+}
+
+sayHello("Caliban");
+sayHello("Miranda");
+sayHello("Ferdinand");
