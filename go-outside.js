24e22b71d1d1ae07c925a61e6872b3d4d8298421
MaligneCanyon <grantkaratnyk@hotmail.com>
2017-12-13 20:08:19 +0000

    added conditionals exercise

---
+++ go-outside.js
@@ -0,0 +1,22 @@
+var raining = false;
+// var cold = false;
+var temperature = -273.15;
+
+
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea …");
+} else {
+  if (!raining) {
+    console.log("Leave your umbrella at home!");
+  } else {
+    console.log("Don't forget your umbrella!");
+  }
+  if (temperature < 0) {
+    console.log("Make sure you pick out a scarf!");
+  } else if (temperature < 15) {
+    console.log("Short sleeves won't cut it!");
+  } else {
+    console.log("Short sleeves are fine.");
+  }
+  console.log("Now you're ready to go outside!");
+}
